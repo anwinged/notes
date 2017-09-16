c57fcfe5e696484483e8e2eaa9ad8f2c01567778
Anton Vakhrushev <anwinged@ya.ru>
2017-09-16 15:57:50 +0300

    Add create link

--- client/components/NoteList.vue
+++ client/components/NoteList.vue
@@ -1,9 +1,14 @@
 <template>
-  <ul class="list">
-    <li v-for="note in notes" :key="note.id" class="item">
-      <preview :note="note"/>
-    </li>
-  </ul>
+  <div>
+    <nav class="actions">
+      <router-link :to="{ name: 'note_create' }">Create</router-link>
+    </nav>
+    <ul class="list">
+      <li v-for="note in notes" :key="note.id" class="item">
+        <preview :note="note"/>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
@@ -27,11 +32,12 @@ export default {
     padding: 0;
     overflow-y: scroll;
   }
+  .actions {
+    padding: 10px 20px;
+  }
   .item {
     margin: 0;
     padding: 0;
-    &:not(:first-child) {
-      border-top: 1px solid #bbb;
-    }
+    border-top: 1px solid #bbb;
   }
 </style>
--- client/router.js
+++ client/router.js
@@ -24,7 +24,7 @@ const routes = [
                 components: {
                     list: NoteList,
                     view: NoteView,
-                }
+                },
             },
             {
                 path: 'create',
