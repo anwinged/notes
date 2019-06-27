650733a2db1429185b44a7c035138a2f1998095e
Anton Vakhrushev <anwinged@ya.ru>
2019-06-27 22:49:28 +0300

    Fix deploy

--- deploy.php
+++ deploy.php
@@ -11,9 +11,9 @@ host('notes.vakhrushev.ru')
     ->set('deploy_path', '/var/www/notes')
 ;
 
-host('notes.loc')
+host('192.168.50.10')
     ->user('notes')
-    ->stage('192.168.50.10')
+    ->stage('staging')
     ->set('deploy_path', '/var/www/notes')
     ->addSshOption('UserKnownHostsFile', '/dev/null')
     ->addSshOption('StrictHostKeyChecking', 'no')
@@ -31,93 +31,32 @@ set('keep_releases', 2);
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
 
-// Symfony build set
-set('symfony_env', 'prod');
-
-// Symfony executable and variable directories
-set('bin_dir', 'bin');
-set('var_dir', 'var');
-
-// Symfony shared dirs
-set('shared_dirs', ['var/logs', 'var/sessions']);
-
-// Symfony writable dirs
-set('writable_dirs', ['var/cache', 'var/logs', 'var/sessions']);
-
-// Clear paths
-set('clear_paths', ['web/app_*.php', 'web/config.php']);
-
-// Assets
-set('assets', ['web/css', 'web/images', 'web/js']);
-
-// Excluded dirs for upload
-set('upload_excluded_dirs', [
-    '.git',
-    'ansible',
-    'client',
-    'node_modules',
-]);
-
-// Environment vars
-set('env', function () {
-    return [
-        'SYMFONY_ENV' => get('symfony_env'),
-        'APP_ENV' => get('symfony_env'),
-    ];
+task('deploy:copy_env', function () {
+    within('{{release_path}}', function () {
+        run('cp -f /home/notes/.env .');
+    });
 });
 
-// Symfony console bin
-set('bin/console', function () {
-    return sprintf('{{release_path}}/%s/console', trim(get('bin_dir'), '/'));
+task('deploy:build_images', function () {
+    within('{{release_path}}', function () {
+        run('make build');
+    });
 });
 
-// Symfony console opts
-set('console_options', function () {
-    $options = '--no-interaction --env={{symfony_env}}';
-    return get('symfony_env') !== 'prod' ? $options : sprintf('%s --no-debug', $options);
+task('deploy:up_containers', function () {
+    within('{{release_path}}', function () {
+        run('make up');
+        run('sleep 30');
+    });
 });
 
-// Binary for npm
-set('bin/npm', function () {
-    return locateBinaryPath('npm');
-});
 
-// Directory for building application
-set('build_dir', '/tmp/build-notes');
-
-/**
- * Clear Cache
- */
-task('deploy:cache:clear', function () {
-    run('{{bin/php}} {{bin/console}} cache:clear {{console_options}} --no-warmup');
-})->desc('Clear cache');
-
-/**
- * Warm up cache
- */
-task('deploy:cache:warmup', function () {
-    run('{{bin/php}} {{bin/console}} cache:warmup {{console_options}}');
-})->desc('Warm up cache');
-
-/**
- * Migrate database
- */
-task('database:migrate', function () {
-    run('{{bin/php}} {{bin/console}} doctrine:migrations:migrate {{console_options}} --allow-no-migration');
-})->desc('Migrate database');
-
-/**
- * Install node packages
- */
-task('assets:npm:install', function () {
-    run('cd {{release_path}} && {{bin/npm}} install');
-});
-
-/**
- * Build assets
- */
-task('assets:npm:build', function () {
-    run('cd {{release_path}} && {{bin/npm}} run build-prod');
+task('deploy:application', function () {
+    within('{{release_path}}', function () {
+        run('make migrate');
+        run('make reindex-search-db');
+        run('make console A="cache:clear"');
+    });
 });
 
 // --------------------------------------------------------
@@ -125,57 +64,22 @@ task('assets:npm:build', function () {
 // --------------------------------------------------------
 
 /**
- * Build app sources
+ * Main task
  */
-task('build', function () {
-    set('deploy_path', get('build_dir'));
-    invoke('deploy:info');
-    invoke('deploy:prepare');
-    invoke('deploy:release');
-    invoke('deploy:update_code');
-    invoke('deploy:vendors');
-    invoke('deploy:clear_paths');
-    invoke('assets:npm:install');
-    invoke('assets:npm:build');
-    invoke('deploy:symlink');
-})->local();
-
-/**
- * Upload app sources on remote host
- */
-task('upload', function () {
-    $excluded = array_map(function ($dir) {
-        return sprintf('--exclude "%s"', $dir);
-    }, get('upload_excluded_dirs'));
-    upload(get('build_dir') . '/current/', '{{release_path}}', [
-        'options' => $excluded,
-    ]);
-});
-
-/**
- * Prepare app sources for production
- */
-task('release', [
-    'deploy:prepare',
-    'deploy:release',
-    'upload',
-    'deploy:shared',
-    'deploy:cache:clear',
-    'deploy:cache:warmup',
-    'database:migrate',
-    'deploy:symlink',
-]);
-
-/**
- * Main task: deploy application
- */
-desc('Deploy your project');
 task('deploy', [
-    'build',
-    'release',
+    'deploy:info',
+    'deploy:prepare',
+    'deploy:lock',
+    'deploy:release',
+    'deploy:update_code',
+    'deploy:copy_env',
+    'deploy:build_images',
+    'deploy:up_containers',
+    'deploy:application',
+    'deploy:unlock',
     'cleanup',
-    'success',
 ]);
 
-// If deploy fails automatically unlock.
+after('deploy', 'success');
+
 after('deploy:failed', 'deploy:unlock');
